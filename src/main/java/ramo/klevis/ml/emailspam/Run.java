c07aeda99c8b5922f726f7799b3b9bcc543ca418
klevisgramo@gmail.com <Labelo4862?!BIBU>
2017-09-26 23:47:40 +0200

    stable

--- src/main/java/ramo/klevis/ml/emailspam/Run.java
+++ src/main/java/ramo/klevis/ml/emailspam/Run.java
@@ -1,28 +1,12 @@
 package ramo.klevis.ml.emailspam;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.mllib.classification.LogisticRegressionModel;
-import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS;
-import org.apache.spark.mllib.evaluation.MulticlassMetrics;
-import org.apache.spark.mllib.regression.LabeledPoint;
-import scala.Tuple2;
-
-import java.io.File;
-import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 /**
  * Created by klevis.ramo on 9/22/2017.
  */
 public class Run {
 
     public static void main(String[] args) throws Exception {
-        new EmailUI(new LogisticRegression(8000));
+        new EmailUI(new LogisticRegression(10000));
 
     }
 
